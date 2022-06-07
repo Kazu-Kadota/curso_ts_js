305c247ac5aefca338ead903149ac2dde7c35c8f
Kazuyuki <kadota.kazuyuki@gmail.com>
2022-06-07 16:44:01 -0300

    Add new classes

---
+++ Section_3_Logica_Programacao/Aula_For_In/index.js
@@ -0,0 +1,20 @@
+// const frutas = ['Pera', 'Maça', 'Uva'];
+
+// for (let i = 0; i < frutas.lenght; i++) {
+//     console.log(frutas[i]);
+// }
+
+// for (let n in frutas) {
+//     console.log(n);
+//     console.log(frutas[n]);
+// }
+
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+    idade: 30
+};
+
+for (let chave in pessoa) {
+    console.log(pessoa[chave]);
+}
