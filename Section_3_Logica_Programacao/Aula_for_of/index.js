c7c394b82b1ba1b13f5c44fb4f7ad837df136442
Kazuyuki <kadota.kazuyuki@gmail.com>
2022-06-08 14:49:33 -0300

    add: class done on 05-08

--- Section_3_Logica_Programacao/Aula_for_of/index.js
+++ Section_3_Logica_Programacao/Aula_for_of/index.js
@@ -1,1 +1,35 @@
 const nome = 'Luiz Otavio';
+const nome1 = [
+    'Luiz',
+    'Otávio'
+];
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+
+for (let i in nome) {
+    console.log(i, nome[i]);
+}
+
+console.log("########")
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+console.log("########")
+
+for (let i in nome1) {
+    console.log(nome1[i]);
+}
+
+console.log("########")
+
+for (let valor of nome1) {
+    console.log(valor);
+}
+
+nome1.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+})
