4ed4f0e79e7c0d87a03f001c41d00643519aef65
Kazuyuki <kadota.kazuyuki@gmail.com>
2022-06-08 14:49:33 -0300

    add: class done on 06-08

--- Section_3_Logica_Programacao/Aula13_Atribuicao_via_desestruturacao/index.js
+++ Section_3_Logica_Programacao/Aula13_Atribuicao_via_desestruturacao/index.js
@@ -1,8 +1,13 @@
-let a = 'A'; // B
-let b = 'B'; // C
-let c = 'C'; // A
+// let a = 'A'; // B
+// let b = 'B'; // C
+// let c = 'C'; // A
 
-const letras = [b, c, a];
-[a, b, c] = letras;
+// const letras = [b, c, a];
+// [a, b, c] = letras;
 
-console.log(a, b, c);
+// console.log(a, b, c);
+
+//               0  1  2  3  4  5  6  7  8
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+
+console.log(numeros);
