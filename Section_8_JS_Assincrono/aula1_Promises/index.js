9ea478d2833088bbf213fd1ac6da23138853769d
Kazuyuki <kadota.kazuyuki@gmail.com>
2022-06-10 13:33:21 -0300

    add: class done on 10-06

--- Section_8_JS_Assincrono/aula1_Promises/index.js
+++ Section_8_JS_Assincrono/aula1_Promises/index.js
@@ -4,12 +4,34 @@ function rand(min, max) {
     return Math.floor(Math.random() * (max-min) + min);
 }
 
-function esperaAi(msg, tempo, cb) {
-    setTimeout(() => {
-        console.log(msg);
-    }, tempo);
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg);
+            reject('bad value');
+        }, tempo);    
+    });
 }
 
-esperaAi('Frase 1', rand(1, 3));
-esperaAi('Frase 2', rand(1, 3));
-esperaAi('Frase 3', rand(1, 3));
+esperaAi('Conexao com BD', rand(1,3))
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Buscando dados da Base', rand(1, 3));
+    })
+    .then(resposta => {
+        console.log(resposta);
+        return 
+    })
+    .then(resposta => {
+        console.log('Exibe dados na tela');
+    })
+    .catch(e => {
+        console.log('Erro', e);
+    });
+
+// esperaAi('Frase 1', rand(1, 3), funcion() {
+//     esperaAi('Frase 2', rand(1, 3), function() {
+//         esperaAi('Frase 3', rand(1, 3));
+//     });
+// });
+
