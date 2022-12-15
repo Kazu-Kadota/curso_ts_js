0f67a5c34499f85cc3a89d9c618ba554914d6f77
Kazuyuki <kazuyuki.kadota@stone.com.br>
2022-12-15 13:49:41 -0300

    exclude react-aplication-api

--- react-aplication-api/src/store/modules/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import reducer from './example/reducer'
-
-export default combineReducers({
-  reducer,
-})
--- react-application-api/src/pages/login/styled.js
+++ react-application-api/src/pages/login/styled.js
@@ -1,15 +1,5 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1`
-  background: green;
+export const Title = styled.h1``
 
-  small {
-    font-size: 12px;
-    margin-left: 15px;
-    color: #999;
-  }
-`
-
-export const Paragrafo = styled.p`
-  font-size: 80px
-`
+export const Paragrafo = styled.p``
